5ce88044c1056057536d63518cc60e96257b27ea
Prahladh Raja <pnamboorkrishnam@wpi.edu>
2023-04-02 21:02:00 -0400

    Redesigned Homepage

--- src/main/java/edu/wpi/teamname/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamname/controllers/HomeController.java
@@ -1,16 +1,72 @@
 package edu.wpi.teamname.controllers;
 
+import static edu.wpi.teamname.navigation.Screen.PATHFINDING;
+
 import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
 
 public class HomeController {
 
-  @FXML MFXButton navigateButton;
+  @FXML MFXButton mealDeliveryButton;
+  @FXML MFXButton reserveRoomButton;
+
+  @FXML MFXButton loginButton;
+
+  @FXML ButtonBar buttonBar;
+
+  @FXML MenuButton app;
+  @FXML Button buttonBarPathfinding;
+  @FXML Button buttonBarReserveRoom;
+  @FXML Button buttonBarMealDelivery;
+
+  @FXML MenuItem exitOption;
+
+  @FXML ImageView helpIcon;
+
+  @FXML MFXButton homeToPathfindingButton;
 
   @FXML
   public void initialize() {
-    navigateButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
+    // Adding the menu option to exit application
+
+    mealDeliveryButton.setOnMouseClicked(event -> goToMealPage());
+    reserveRoomButton.setOnMouseClicked(event -> goToRoomPage());
+    loginButton.setOnMouseClicked(event -> goToSignagePage());
+    helpIcon.setOnMouseClicked(event -> goToHelpPage());
+    exitOption.setOnAction(event -> exitApplication());
+    homeToPathfindingButton.setOnMouseClicked(event -> Navigation.navigate(PATHFINDING));
+
+    // Menu bar button handlers
+    buttonBarPathfinding.setOnMouseClicked(event -> Navigation.navigate(PATHFINDING));
+    buttonBarMealDelivery.setOnMouseClicked(event -> goToMealPage());
+    buttonBarReserveRoom.setOnMouseClicked(event -> goToRoomPage());
+  }
+
+  public void goToRoomPage() {
+    Navigation.navigate(Screen.SERVICE_REQUEST);
+  }
+
+  public void goToMealPage() {
+    Navigation.navigate(Screen.SERVICE_REQUEST);
+  }
+
+  public void goToSignagePage() {
+    Navigation.navigate(Screen.SIGNAGE_PAGE);
+  }
+
+  public void exitApplication() {
+    Platform.exit();
+  }
+
+  public void goToHelpPage() {
+    Navigation.navigate(Screen.HELP_PAGE);
   }
 }
--- src/main/java/edu/wpi/teamname/navigation/Screen.java
+++ src/main/java/edu/wpi/teamname/navigation/Screen.java
@@ -3,6 +3,10 @@ package edu.wpi.teamname.navigation;
 public enum Screen {
   ROOT("views/Root.fxml"),
   HOME("views/Home.fxml"),
+  SIGNAGE_PAGE("views/SignagePage.fxml"),
+  HELP_PAGE("views/HelpPage.fxml"),
+
+  PATHFINDING("views/Pathfinding.fxml"),
   SERVICE_REQUEST("views/ServiceRequest.fxml");
 
   private final String filename;
