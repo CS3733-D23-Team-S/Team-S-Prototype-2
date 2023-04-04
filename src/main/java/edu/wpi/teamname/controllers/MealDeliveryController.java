7d1553cf6747eebb33816a4c8c39c0b1f283aa19
kashvi <skashvi1880@gmail.com>
2023-04-04 15:10:55 -0400

    adding working meal delivery(back button works)

--- src/main/java/edu/wpi/teamname/controllers/MealDeliveryController.java
+++ src/main/java/edu/wpi/teamname/controllers/MealDeliveryController.java
@@ -3,14 +3,41 @@ package edu.wpi.teamname.controllers;
 import edu.wpi.teamname.navigation.Navigation;
 import edu.wpi.teamname.navigation.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import java.util.ArrayList;
 import javafx.fxml.FXML;
+import javafx.scene.layout.HBox;
 
 public class MealDeliveryController {
 
   @FXML MFXButton backButton1;
+  @FXML HBox wf;
+
+  ArrayList<Integer> foodPrice = new ArrayList<Integer>();
 
   @FXML
   public void initialize() {
     backButton1.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+
+    foodPrice.add(3);
+    foodPrice.add(24);
+    foodPrice.add(32);
+    foodPrice.add(20);
+
+    walletFriendly();
+  }
+
+  public void walletFriendly() {
+    for (int i = 0; i < foodPrice.size(); i++) {
+      if (foodPrice.get(i) < 30) {
+        MFXButton btn = new MFXButton();
+        btn.setId(foodPrice.get(i).toString());
+        String button = foodPrice.get(i).toString();
+        btn.setText(foodPrice.get(i).toString());
+        btn.setMaxWidth(103);
+        btn.setMaxHeight(87);
+        wf.getChildren().add(btn);
+        btn.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+      }
+    }
   }
 }
