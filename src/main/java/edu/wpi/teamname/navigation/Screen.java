c71c38f0d815b9c6efb1339d1b6dc65b92afc0ad
Anthony <atitcombe13@gmail.com>
2023-04-04 13:40:40 -0400

    adding working meal delivery(back button doesn't work)

--- src/main/java/edu/wpi/teamname/navigation/Screen.java
+++ src/main/java/edu/wpi/teamname/navigation/Screen.java
@@ -4,7 +4,11 @@ public enum Screen {
   ROOT("views/Root.fxml"),
   HOME("views/Home.fxml"),
   MEAL_DELIVERY1("views/ServiceRequestPages/MealDelivery1.fxml"),
-  SERVICE_REQUEST("views/ServiceRequestPages/ServiceRequest.fxml");
+  SERVICE_REQUEST("views/ServiceRequestPages/ServiceRequest.fxml"),
+  ROOM_BOOKING("views/RoomBooking.fxml"),
+  ROOM_BOOKING_DETAILS("views/RoomBookingDetails.fxml"),
+  ROOM_BOOKING_CONFIRMATION("views/RoomBookingConfirmation.fxml");
+  ;
 
   private final String filename;
 
