804d9889ba535ab78e35392a9850c6eda7cc134d
TBrown2024 <tbrown@wpi.edu>
2023-04-03 20:03:29 -0400

    Added dbConnection

--- src/main/java/edu/wpi/teamname/Database/Map/NodeDOA_I.java
+++ src/main/java/edu/wpi/teamname/Database/Map/NodeDOA_I.java
@@ -2,7 +2,8 @@ package edu.wpi.teamname.Database.Map;
 
 import java.util.List;
 
-public interface NodeDOA_I {
+public interface NodeDOA_I
+{
     public List<Node> getAllStudents();
     public Node getNode(int nodeID);
     public void updateNode(Node node);
