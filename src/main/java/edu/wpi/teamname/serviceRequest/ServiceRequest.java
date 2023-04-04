1b86de3ff60766900d004cc52cd177422a2af363
Fatducky1 <NathanielRubinM@gmail.com>
2023-04-03 20:24:21 -0400

    commit

---
+++ src/main/java/edu/wpi/teamname/serviceRequest/ServiceRequest.java
@@ -0,0 +1,18 @@
+package edu.wpi.teamname.serviceRequest;
+
+public class ServiceRequest {
+    enum requestType{};
+    enum Status{};
+    String asignee;
+
+    public ServiceRequest() {
+
+    }
+
+    public String toString() {
+        return null;
+    }
+
+
+
+}
