d4bae7859580a4ad09bd9588104bfaccbfa10615
Anthony <atitcombe13@gmail.com>
2023-04-05 12:10:54 -0400

    added empty test page

---
+++ src/test/java/edu/wpi/teamname/RoomBookingTest.java
@@ -0,0 +1,18 @@
+package edu.wpi.teamname;
+
+import edu.wpi.teamname.Database.ServiceRequests.ConferenceRoom.ConfRoomRequest;
+import edu.wpi.teamname.Database.ServiceRequests.ConferenceRoom.RoomRequestDAO;
+import org.junit.jupiter.api.Test;
+
+public class RoomBookingTest {
+
+
+    public RoomBookingTest() {
+        RoomRequestDAO roomRequestDAO = RoomRequestDAO.getInstance();
+
+
+    }
+
+    @Test
+    public void test() {}
+}
