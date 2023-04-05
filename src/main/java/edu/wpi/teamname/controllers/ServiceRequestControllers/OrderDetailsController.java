7e26c148c6f5f506a5d84831c0f24c92cae0166c
kashvi <skashvi1880@gmail.com>
2023-04-04 20:46:38 -0400

    updated meal delivery

---
+++ src/main/java/edu/wpi/teamname/controllers/ServiceRequestControllers/OrderDetailsController.java
@@ -0,0 +1,17 @@
+package edu.wpi.teamname.controllers.ServiceRequestControllers;
+
+import edu.wpi.teamname.navigation.Navigation;
+import edu.wpi.teamname.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+
+public class OrderDetailsController {
+
+  @FXML MFXButton back2;
+
+  @FXML
+  public void initialize() {
+
+    back2.setOnMouseClicked(event -> Navigation.navigate(Screen.MEAL_DELIVERY1));
+  }
+}
