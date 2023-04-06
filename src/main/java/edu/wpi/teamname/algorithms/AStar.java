a1ed2ce6b525e85cfd2ec04e975f0b9f407de63d
Nikesh <nikeshwalling@gmail.com>
2023-04-05 20:11:53 -0400

    Fixing the git ignore

--- src/main/java/edu/wpi/teamname/algorithms/AStar.java
+++ src/main/java/edu/wpi/teamname/algorithms/AStar.java
@@ -1,99 +1,98 @@
 package edu.wpi.teamname.algorithms;
 
 import edu.wpi.teamname.Database.Map.*;
-
 import java.util.*;
 
 public class AStar {
 
-	private NodeDaoImpl nodeDao;
-	private EdgeDaoImpl edgeDao;
-	private LocationDoaImpl locationDoa;
-	private MoveDaoImpl moveDao;
+  private NodeDaoImpl nodeDao;
+  private EdgeDaoImpl edgeDao;
+  private LocationDoaImpl locationDoa;
+  private MoveDaoImpl moveDao;
 
-	public AStar() {
-		nodeDao = NodeDaoImpl.getInstance();
-		edgeDao = EdgeDaoImpl.getInstance();
-		locationDoa = LocationDoaImpl.getInstance();
-		moveDao = MoveDaoImpl.getInstance();
-	}
+  public AStar() {
+    nodeDao = NodeDaoImpl.getInstance();
+    edgeDao = EdgeDaoImpl.getInstance();
+    locationDoa = LocationDoaImpl.getInstance();
+    moveDao = MoveDaoImpl.getInstance();
+  }
 
-	/**
-	 * A* Using strings that represents either the long name of the nodes or the node ids
-	 *
-	 * @param s
-	 * @param e
-	 * @return
-	 */
-	public ArrayList<Integer> findPath(String s, String e) {
-		Node start, end;
-		if (s.matches(".*\\d.*")) start = nodeDao.getNode(Integer.parseInt(s));
-		else start = nodeDao.getNode(moveDao.getMoves().get(s).getNodeID());
-		if (e.matches(".*\\d.*")) end = nodeDao.getNode(Integer.parseInt(e));
-		else end = nodeDao.getNode(moveDao.getMoves().get(e).getNodeID());
+  /**
+   * A* Using strings that represents either the long name of the nodes or the node ids
+   *
+   * @param s
+   * @param e
+   * @return
+   */
+  public ArrayList<Integer> findPath(String s, String e) {
+    Node start, end;
+    if (s.matches(".*\\d.*")) start = nodeDao.getNode(Integer.parseInt(s));
+    else start = nodeDao.getNode(moveDao.getMoves().get(s).getNodeID());
+    if (e.matches(".*\\d.*")) end = nodeDao.getNode(Integer.parseInt(e));
+    else end = nodeDao.getNode(moveDao.getMoves().get(e).getNodeID());
 
-		final PriorityQueue<HeuristicNode> nodesYetToSearch =
-				new PriorityQueue<>(10, new HeuristicNode(null, Double.MAX_VALUE));
-		final HashSet<Node> visitedNodes = new HashSet<>();
-		final Map<Node, Node> gotHereFrom = new HashMap<>();
+    final PriorityQueue<HeuristicNode> nodesYetToSearch =
+        new PriorityQueue<>(10, new HeuristicNode(null, Double.MAX_VALUE));
+    final HashSet<Node> visitedNodes = new HashSet<>();
+    final Map<Node, Node> gotHereFrom = new HashMap<>();
 
-		HeuristicNode startHNode = new HeuristicNode(start, calculateWeight(start, end));
-		//    System.out.println(startHNode.node + "\t" + startHNode.weight);
-		nodesYetToSearch.add(startHNode);
-		HeuristicNode currentNode;
+    HeuristicNode startHNode = new HeuristicNode(start, calculateWeight(start, end));
+    //    System.out.println(startHNode.node + "\t" + startHNode.weight);
+    nodesYetToSearch.add(startHNode);
+    HeuristicNode currentNode;
 
-		while (nodesYetToSearch.size() != 0) {
-			currentNode = nodesYetToSearch.poll();
-			if (currentNode.node.getNodeID() == (end.getNodeID())) {
-				return constructShortestPath(currentNode.node, gotHereFrom);
-			}
-			//      dbManager.printLocalDatabases();
-			//      System.out.print(currentNode.node.getNodeID());
-			for (int nodeToSearchID : edgeDao.getNeighbors().get(currentNode.node.getNodeID())) {
-				//        System.out.print(nodeToSearchID + "\t");
-				Node nodeToSearch = nodeDao.getNodes().get(nodeToSearchID);
-				if (!visitedNodes.contains(nodeToSearch)) {
-					double weight = calculateWeight(nodeToSearch, end);
-					nodesYetToSearch.add(new HeuristicNode(nodeToSearch, weight));
-					gotHereFrom.put(nodeToSearch, currentNode.node);
-				}
-			}
-			visitedNodes.add(currentNode.node);
-		}
-		return null;
-	}
+    while (nodesYetToSearch.size() != 0) {
+      currentNode = nodesYetToSearch.poll();
+      if (currentNode.node.getNodeID() == (end.getNodeID())) {
+        return constructShortestPath(currentNode.node, gotHereFrom);
+      }
+      //      dbManager.printLocalDatabases();
+      //      System.out.print(currentNode.node.getNodeID());
+      for (int nodeToSearchID : edgeDao.getNeighbors().get(currentNode.node.getNodeID())) {
+        //        System.out.print(nodeToSearchID + "\t");
+        Node nodeToSearch = nodeDao.getNodes().get(nodeToSearchID);
+        if (!visitedNodes.contains(nodeToSearch)) {
+          double weight = calculateWeight(nodeToSearch, end);
+          nodesYetToSearch.add(new HeuristicNode(nodeToSearch, weight));
+          gotHereFrom.put(nodeToSearch, currentNode.node);
+        }
+      }
+      visitedNodes.add(currentNode.node);
+    }
+    return null;
+  }
 
-	private double calculateWeight(Node start, Node target) {
-		return Math.sqrt(
-				Math.pow((start.getXCoord() - target.getXCoord()), 2)
-				+ Math.pow((start.getYCoord() - target.getYCoord()), 2));
-	}
+  private double calculateWeight(Node start, Node target) {
+    return Math.sqrt(
+        Math.pow((start.getXCoord() - target.getXCoord()), 2)
+            + Math.pow((start.getYCoord() - target.getYCoord()), 2));
+  }
 
-	private ArrayList<Integer> constructShortestPath(Node currentNode, Map<Node, Node> gotHereFrom) {
-		final ArrayList<Integer> pathTaken = new ArrayList<>();
-		while (gotHereFrom.get(currentNode) != null) {
-			pathTaken.add(currentNode.getNodeID());
-			currentNode = gotHereFrom.get(currentNode);
-		}
-		pathTaken.add(currentNode.getNodeID());
-		Collections.reverse(pathTaken);
-		return pathTaken;
-	}
+  private ArrayList<Integer> constructShortestPath(Node currentNode, Map<Node, Node> gotHereFrom) {
+    final ArrayList<Integer> pathTaken = new ArrayList<>();
+    while (gotHereFrom.get(currentNode) != null) {
+      pathTaken.add(currentNode.getNodeID());
+      currentNode = gotHereFrom.get(currentNode);
+    }
+    pathTaken.add(currentNode.getNodeID());
+    Collections.reverse(pathTaken);
+    return pathTaken;
+  }
 
-	//
-	// pathTaken.put(currentNode.getNodeID(),moveDao.getNodeToLoc().get(currentNode.getNodeID()).get(1));
-	static class HeuristicNode implements Comparator<HeuristicNode> {
-		Node node;
-		double weight;
+  //
+  // pathTaken.put(currentNode.getNodeID(),moveDao.getNodeToLoc().get(currentNode.getNodeID()).get(1));
+  static class HeuristicNode implements Comparator<HeuristicNode> {
+    Node node;
+    double weight;
 
-		public HeuristicNode(Node node, double weight) {
-			this.node = node;
-			this.weight = weight;
-		}
+    public HeuristicNode(Node node, double weight) {
+      this.node = node;
+      this.weight = weight;
+    }
 
-		@Override
-		public int compare(HeuristicNode o1, HeuristicNode o2) {
-			return Double.compare(o1.weight, o2.weight);
-		}
-	}
+    @Override
+    public int compare(HeuristicNode o1, HeuristicNode o2) {
+      return Double.compare(o1.weight, o2.weight);
+    }
+  }
 }
